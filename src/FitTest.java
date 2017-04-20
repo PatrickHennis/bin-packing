7923d637ec06b93d53cab27e65d28b7f00b11540
PatrickHennis <patrickhennis@gmail.com>
2017-04-20 17:20:44 -0400

    added some test cases

--- src/FitTest.java
+++ src/FitTest.java
@@ -8,16 +8,26 @@ public class FitTest {
 	public void testBestFit() {
 		double[] bins = { 0.2, 0.8, 0.4, 0.6, 0.1, 0.9, 0.1, 0.5, 0.5, 0.7, 0.3, 0.8, 0.2, 0.4, 0.6, 0.9, 0.1, 0.6, 0.4 };
 		int size = bins.length;
-		double[] used = new double[size];
-		int numBins = 0;
-		int binCost = FirstFit.bestFit(bins, size, used, numBins, 0, 100);
-		int totalBinWeight = 0;
+		int binCost = OtherFits.BestFit(bins);
+		double totalBinWeight = 0;
 		for (int i = 0; i < size; i++) {
 			totalBinWeight += bins[i];
 		}
-		int expected = (int)Math.ceil(totalBinWeight/1);
-		assertEquals("cost not same: ", 10, binCost);
-		
+		int expected = (int)Math.ceil(totalBinWeight);
+		assertEquals("cost not same: ", expected, binCost);
+	}
+	
+	@Test
+	public void testBestFit2() {
+		double[] bins = { 0.2, 0.8, 0.4, 0.6, 0.1, 0.9, 0.1, 0.5, 0.9, 0.7, 0.3, 0.8, 0.2, 0.9, 0.6, 0.9, 0.2, 0.6, 0.4 };
+		int size = bins.length;
+		int binCost = OtherFits.BestFit(bins);
+		double totalBinWeight = 0;
+		for (int i = 0; i < size; i++) {
+			totalBinWeight += bins[i];
+		}
+		int expected = (int)Math.ceil(totalBinWeight);
+		assertEquals("cost not same: ", expected, binCost);
 	}
 
 }
